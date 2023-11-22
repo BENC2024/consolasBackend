1a749cb79b25be8eb61a3f8c1df46fba5c035883
brahian github <brahiangithub@gmail.com>
2023-11-22 14:00:37 -0500

    v2

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const puerto = 3000;
 const consolaRoutes = require("../routes/consola.routes");
+const cors = require('cors');
 
+app.use(cors()); //MUY IMPORTANTE EL ORDEN DE EJECUCION DEL USE
 app.use(express.json()); // Utilice el formato json //MALDITOS PARENTESIS
 app.use(consolaRoutes);
 
